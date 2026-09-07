094689060879dd8eea318e04b9906f1364db7483
Salman Alaraby <salmanala@github.com>
2026-09-07 08:07:29 -0400

    Implement login and logout functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,7 @@
 # from datetime import datetime
 
 from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -63,3 +63,7 @@ def login_user(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+def logout_request(request):
+    logout(request)
+    return JsonResponse({"status": "Logged out"})
